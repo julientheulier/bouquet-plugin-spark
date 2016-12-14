581ec968da7e66638f42825d3cbab62ef130212c
hoahd <hoa@squidsolutions.com>
2016-12-14 16:34:23 +0100

    T1828

--- src/com/squid/core/jdbc/vendor/spark/SparkDriverShim.java
+++ src/com/squid/core/jdbc/vendor/spark/SparkDriverShim.java
@@ -21,55 +21,34 @@
  * you and Squid Solutions (above licenses and LICENSE.txt included).
  * See http://www.squidsolutions.com/EnterpriseBouquet/
  *******************************************************************************/
+
 package com.squid.core.jdbc.vendor.spark;
 
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.sql.Connection;
 import java.sql.Driver;
-import java.util.ArrayList;
+import java.sql.SQLException;
+import java.util.Properties;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.squid.core.database.impl.ConnectionShim;
+import com.squid.core.database.impl.DefaultDriverShim;
 
-import com.squid.core.database.impl.DriverShim;
-import com.squid.core.database.plugins.BaseBouquetPlugin;
+public class SparkDriverShim extends DefaultDriverShim {
 
-public class SparkBouquetPlugin extends BaseBouquetPlugin {
-
-	private static final Logger logger = LoggerFactory.getLogger(SparkBouquetPlugin.class);
-
-	public static final String driverName = "org.apache.hive.jdbc.HiveDriver";
-
-	@Override
-	public void loadDriver() {
-		URL[] paths = new URL[1];
-		paths[0] = this.getClass().getProtectionDomain().getCodeSource().getLocation();
-
-		// load the driver within an isolated classLoader
-		this.driverCL = new URLClassLoader(paths);
-		ClassLoader rollback = Thread.currentThread().getContextClassLoader();
-		Thread.currentThread().setContextClassLoader(driverCL);
-
-		this.drivers = new ArrayList<Driver>();
-
-		try {
-			Driver driver = (Driver) Class.forName(driverName, true, driverCL).newInstance();
-			drivers.add(driver);
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-			logger.error("Could not load driver " + driverName + " for Spark plugin");
-			;
-		}
-		Thread.currentThread().setContextClassLoader(rollback);
-
+	public SparkDriverShim(Driver d) {
+		super(d);
 	}
 
 	@Override
-	public ArrayList<DriverShim> getDrivers() {
-		ArrayList<DriverShim> shims = new ArrayList<DriverShim>();
-		for (Driver d : this.drivers) {
-			shims.add(new SparkDriverShim(d));
+	public Connection connect(String u, Properties p) throws SQLException {
+		// make sure to use the driverLoader ctx
+		ClassLoader loader = this.driver.getClass().getClassLoader();
+		ClassLoader rollback = Thread.currentThread().getContextClassLoader();
+		Thread.currentThread().setContextClassLoader(loader);
+		try {
+			return new ConnectionShim(this.driver.connect(u, p));
+		} finally {
+			Thread.currentThread().setContextClassLoader(rollback);
 		}
-		return shims;
 	}
 
 }
